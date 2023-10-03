fc4650da7a7c37af5f650e6969ac86f17962d586
unknown <hirohama0927@gmail.com>
2023-10-03 19:44:09 +0900

    shopping_guide

---
+++ assets/modal-window.js
@@ -0,0 +1,16 @@
+  const targetmodal = document.querySelector('.modal-display-wrapper');
+  const target = document.querySelectorAll('.modal-image');
+  const closeBtn = document.querySelector('.close-btn');
+
+  window.addEventListener('load', () => {
+    
+    target.forEach(function(e){
+      e.addEventListener('click', () => {
+        targetmodal.querySelector('img').src=e.src;
+        targetmodal.classList.add('active');
+    });
+    });
+  });
+  closeBtn.addEventListener('click', () => {
+    targetmodal.classList.remove('active');
+  });
