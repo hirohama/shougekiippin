47ef82e30ea81aa789adbc886dad54db9ded1977
unknown <hirohama0927@gmail.com>
2023-10-28 12:43:10 +0900

    mobile_smp1027_mv

--- assets/_js_main.js
+++ assets/_js_main.js
@@ -277,16 +277,13 @@ if($('#mv').length){
          $(this).attr('src', '');
         // 読み込みを監視
          $(this).on('load', function() {
-         console.log('画像の読み込みが完了しました');
+             console.log('画像の読み込みが完了しました');
+             $(this).attr('src', this_src);
+             loaded+=1;
+             if($imglength==loaded){
+                mv_animation();
+             }
         });
-        // パスを再設定
-         $(this).attr('src', this_src);
-
-         loaded+=1;
-         if($imglength==loaded){
-            console.log("よみこまれました");
-            mv_animation();
-         }
     });
     function mv_animation_start(){
         timel.fromTo('#mv_loader',{
